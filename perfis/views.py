dc0761d20832f196242d62ad7d678cf5e4972423
Wagner Trindade <wtrindades@hotmail.com>
2015-05-16 18:09:26 -0300

    Aula 12 - Autenticando e Autorizando nossos Usuários

--- perfis/views.py
+++ perfis/views.py
@@ -1,26 +1,33 @@
 from django.shortcuts import render, redirect
 from perfis.models import Perfil, Convite
+from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
+@login_required
 def index(resquest):
 	return render(resquest, 'index.html', { "perfis" : Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(resquest)})
 
+@login_required
 def exibir(resquest, perfil_id):
 	perfil = Perfil.objects.get(id=perfil_id)
 	perfil_logado = get_perfil_logado(resquest)
 	ja_eh_contato = perfil in perfil_logado.contatos.all() 
 	return render(resquest, 'perfil.html', { "perfil" : perfil, "perfil_logado" : perfil_logado , "ja_eh_contato" : ja_eh_contato})
 
+@permission_required('perfis.add_convite', raise_exception=True)
+@login_required
 def convidar(resquest, perfil_id):
 	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
 	perfil_logado = get_perfil_logado(resquest)
 	perfil_logado.convidar(perfil_a_convidar)
 	return redirect('index')
 
+@login_required
 def aceitar(resquest, convite_id):
 	convite = Convite.objects.get(id=convite_id)
 	convite.aceitar()
 	return redirect('index')
 
+@login_required
 def get_perfil_logado(resquest):
-	return Perfil.objects.get(id='1')
+	return resquest.user.perfil
